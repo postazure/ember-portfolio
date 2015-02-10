f726592fa03c73f3ecc90fe010f784e7a84e353d
Max Gordon <gordonmaxc@gmail.com>
2015-02-10 15:11:19 -0800

    active tab

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,11 @@
 window.App = Ember.Application.create();
+$(document).ready(function () {
+    $("#menu-bar").on("click", 'a', function () {
+      $("a.item.active").removeClass("active")
+      var selection = $(this)
+      setTimeout(function(){
+        $('.ui.video').video();
+        selection.addClass("active")
+      }, 10);
+    })
+})
--- javascripts/router.js
+++ javascripts/router.js
@@ -1,5 +1,5 @@
 App.Router.map(function () {
-  this.route("about")
+  // this.route("about")
   this.route("projects")
   this.route("games")
 })
